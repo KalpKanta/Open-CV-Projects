c59383497c36dbdf0a2dffec1dd509a80cf54a84
KalpKanta <Exoplayz72@gmail.com>
2025-04-05 18:39:58 +0100

    Add files via upload

--- invisibility cloak.py
+++ invisibility cloak.py
@@ -20,4 +20,23 @@ while vVideo.isOpened():
     img = np.flip(image, axis = 1)
 
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lowerRed = np.array)
+    lowerRed = np.array([100, 40, 40])
+    upRed = np.array([100, 255, 255])
+    mask1 = cv2.inRange(hsv_img, lowerRed, upRed)
+    
+    hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lowerRed = np.array([150, 40, 40])
+    upRed = np.array([180, 255, 255])
+    mask2 = cv2.inRange(hsv_img, lowerRed, upRed)
+
+    mask1 = mask1 + mask2
+    mask1 = cv2.morphologyEx(mask1, cv2.MORPH_OPEN, np.ones((3,3), np.uint8), iterations = 1)
+    mask1 =cv2.dilate(mask1, np.ones((3,3), np.uint8), iterations = 1)
+    mask2 = cv2.bitwise_not(mask1)
+    result1 = cv2.bitwise_and(background, background, mask = mask1)
+    result2 = cv2.bitwise_and(img, img, mask = mask2)
+    final_output = cv2.addWeighted(result1, 1, result2, 1, 0)
+    cv2.imshow("Invisible mask", final_output)
+    K = cv2.waitKey(10)
+    if K == 27:
+        break
